765b599e7ad545fe6b835e3ce519af1364a936e0
Enrique <engarcia@acid.cl>
2017-06-21 17:55:12 -0400

    Move to spree correct path

--- app/models/copayment_relation.rb
+++
@@ -1,6 +0,0 @@
-class CopaymentRelation < ActiveRecord::Base
-  belongs_to :relatable, class_name: 'Spree::Variant', touch: true
-  belongs_to :related_to, class_name: 'Spree::Variant'
-
-  validates :relatable_id, uniqueness: { scope: [:related_to_id] }
-end
