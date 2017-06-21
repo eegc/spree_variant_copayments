da5b05f50cf66ed0a5b38314b070fd5589d88544
Enrique <engarcia@acid.cl>
2017-06-21 17:35:29 -0400

    Fix copayment relation migration

--- db/migrate/20170621195409_create_copayment_relations.rb
+++ db/migrate/20170621195409_create_copayment_relations.rb
@@ -1,12 +1,16 @@
 class CreateCopaymentRelations < ActiveRecord::Migration
   def change
     create_table :copayment_relations do |t|
-      t.references :relatable, index: true, foreign_key: true
-      t.references :related_to, index: true, foreign_key: true
+      t.integer :relatable_id
+      t.integer :related_to_id
       t.decimal :discount_amount, precision: 8, scale: 2, default: 0.0
       t.integer :position
 
       t.timestamps null: false
     end
+
+    add_index :copayment_relations, :relatable_id
+    add_index :copayment_relations, :related_to_id
+    add_index :copayment_relations, [:relatable_id, :related_to_id], name: 'index_copayment_relations_ids'
   end
 end
