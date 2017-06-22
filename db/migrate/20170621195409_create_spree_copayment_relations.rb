41b57971e0a404ea310d177bcba41ab5f3d9bb4a
Enrique <engarcia@acid.cl>
2017-06-22 12:29:42 -0400

    Fix copayment_relations migration

---
+++ db/migrate/20170621195409_create_spree_copayment_relations.rb
@@ -0,0 +1,16 @@
+class CreateSpreeCopaymentRelations < ActiveRecord::Migration
+  def change
+    create_table :spree_copayment_relations do |t|
+      t.integer :relatable_id
+      t.integer :related_to_id
+      t.decimal :discount_amount, precision: 8, scale: 2, default: 0.0
+      t.integer :position
+
+      t.timestamps null: false
+    end
+
+    add_index :spree_copayment_relations, :relatable_id
+    add_index :spree_copayment_relations, :related_to_id
+    add_index :spree_copayment_relations, [:relatable_id, :related_to_id], name: 'index_copayment_relations_ids'
+  end
+end
