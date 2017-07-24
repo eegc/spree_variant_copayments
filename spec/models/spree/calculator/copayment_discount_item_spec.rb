00f305aa1e1e28dc93e8c1a9eb51275c27ea672e
Enrique <engarcia@acid.cl>
2017-07-24 12:07:49 -0400

    Add new specs for action

--- spec/models/spree/calculator/copayment_discount_item_spec.rb
+++ spec/models/spree/calculator/copayment_discount_item_spec.rb
@@ -16,14 +16,17 @@ module Spree
         let(:variant3)  { create(:product, currency: 'CLP').master }
         let(:copayment) { create(:product, currency: 'CLP').master }
 
-        let(:line_item_1) { create(:line_item, variant: variant, order: order, quantity: 2) }
+        let(:line_item_1) { create(:line_item, variant: variant , order: order, quantity: 2) }
         let(:line_item_2) { create(:line_item, variant: copayment, order: order) }
 
-        let(:line_item_3) { create(:line_item, variant: variant2, order: order, quantity: 2) }
+        let(:line_item_3) { create(:line_item, variant: variant2 , order: order, quantity: 2) }
         let(:line_item_4) { create(:line_item, variant: copayment, order: order, quantity: 2) }
 
-        let(:line_item_5) { create(:line_item, variant: variant3, order: order, quantity: 1) }
+        let(:line_item_5) { create(:line_item, variant: variant3 , order: order, quantity: 1) }
         let(:line_item_6) { create(:line_item, variant: copayment, order: order, quantity: 1) }
+
+        let(:line_item_7) { create(:line_item, variant: variant  , order: order, quantity: 1) }
+        let(:line_item_8) { create(:line_item, variant: variant2 , order: order, quantity: 1) }
 
         let(:relation)  { create(:copayment_relation, relatable: variant, related_to: copayment, active: true, discount_amount: 1.0) }
         let(:relation2) { create(:copayment_relation, relatable: variant2, related_to: copayment, active: true, discount_amount: 2.0) }
@@ -64,7 +67,7 @@ module Spree
           expect(subject.compute(line_item_4)).to eq 4
         end
 
-        it "computed with relatable and inactive copayment in cart" do
+        it "computed with relatable and inactive relation in cart" do
           order.line_items = [line_item_5, line_item_6]
           expect(subject.eligible?(line_item_5)).to be false
           expect(subject.eligible?(line_item_6)).to be false
@@ -72,11 +75,23 @@ module Spree
         end
 
         it "computed with relatable and inactive copayment in cart" do
-          order.line_items = [line_item_1, line_item_3, line_item_2]
+          order.line_items = [line_item_1, line_item_5, line_item_2]
           expect(subject.eligible?(line_item_1)).to be false
-          expect(subject.eligible?(line_item_3)).to be false
+          expect(subject.eligible?(line_item_5)).to be false
           expect(subject.eligible?(line_item_2)).to be true
-          expect(subject.compute(line_item_2)).to eq 2
+          expect(subject.compute(line_item_2)).to eq 1
+        end
+
+        it "computed with more than one relatables with relations with different discounts" do
+          # relatable 1 -> copayment 1
+          # relatable 1 -> copayment 2
+          order.line_items = [line_item_7, line_item_8, line_item_4]
+          expect(subject.eligible?(line_item_7)).to be false
+          expect(subject.eligible?(line_item_8)).to be false
+          expect(subject.eligible?(line_item_4)).to be true
+          expect(subject.compute(line_item_7)).to eq 0
+          expect(subject.compute(line_item_8)).to eq 0
+          expect(subject.compute(line_item_4)).to eq 3
         end
       end
     end
